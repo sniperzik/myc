089302c536a627d036f498ea54f15622df02eb6d
Dāvis V a.k.a. Sn^ <sniperzik22@gmail.com>
2016-01-03 23:12:32 +0200

    Delete test.php

--- test.php
+++
@@ -1,1 +0,0 @@
-&copy; <?php echo $r->fyear ?> <a href="/"><?php echo $r->fweb ?></a> &sdot; <?php echo $r->fcb ?> <b><a href="mailto:<?php echo $r->fmail ?>" target="_blank"><?php echo $r->author ?></a>
