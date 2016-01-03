55f9e3373b3909f3d706c4d6556f491ae3abd7e5
Dāvis V a.k.a. Sn^ <sniperzik22@gmail.com>
2016-01-03 22:56:23 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,1 @@
+&copy; <?php echo $r->fyear ?> <a href="/"><?php echo $r->fweb ?></a> &sdot; <?php echo $r->fcb ?> <b><a href="mailto:<?php echo $r->fmail ?>" target="_blank"><?php echo $r->author ?></a>
